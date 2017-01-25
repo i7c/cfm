f965b22e367db51d83f8161ddf23d19b3831cbf7
Constantin Weißer <i7c@posteo.de>
2017-01-25 22:59:56 +0100

    Introduce Formatters in Client Module

---
+++ client/linux/Cfm/Formatter.pm
@@ -0,0 +1,16 @@
+package Cfm::Formatter;
+use strict;
+use warnings FATAL => 'all';
+use Moo;
+use Carp;
+
+
+sub artist {
+    carp "not implemented";
+}
+
+sub artist_list {
+    carp "not implemented";
+}
+
+1;
---
+++ client/linux/Cfm/PrettyFormatter.pm
@@ -0,0 +1,36 @@
+package Cfm::PrettyFormatter;
+use strict;
+use warnings FATAL => 'all';
+use Moo;
+use Text::ASCIITable;
+
+use Cfm::Formatter;
+use Cfm::Artist;
+use Cfm::ArtistList;
+
+extends 'Cfm::Formatter';
+
+
+sub artist {
+    my ($self, $artist) = @_;
+
+    my $table = Text::ASCIITable->new({headingText => $artist->name});
+    $table->setCols("Field", "Value");
+    $table->addRow("Name", $artist->name);
+    $table->addRow("Identifier", $artist->identifier);
+    $table->addRow("Mbid", $artist->mbid);
+    print $table;
+}
+
+sub artist_list {
+    my ($self, $list) = @_;
+
+    my $table = Text::ASCIITable->new({headingText => 'Artists'});
+    $table->setCols("Name", "Identifier", "Mbid");
+    for my $artist (@{$list->elements}) {
+        $table->addRow($artist->name, $artist->identifier, $artist->mbid);
+    }
+    print $table;
+}
+
+1;
