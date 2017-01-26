f2618b9b8440a89caec419feabd89d86caf62fa2
Constantin Weißer <i7c@posteo.de>
2017-01-26 23:47:16 +0100

    Set User-Agent for REST requests in the back-end

---
+++ server/recorder/src/main/java/org/rliz/cfm/musicbrainz/api/UserAgentRequestInterceptor.java
@@ -0,0 +1,22 @@
+package org.rliz.cfm.musicbrainz.api;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * Can be used to set additional headers etc for requests with feign.
+ */
+@Component
+public class UserAgentRequestInterceptor implements RequestInterceptor {
+
+    @Value("${cfm.userAgent}")
+    private String userAgent;
+
+    @Override
+    public void apply(RequestTemplate template) {
+        template.header("User-Agent", userAgent);
+    }
+}
