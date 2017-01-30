3ede52ffccc10cd8ce6f0e080b181f2c0421c149
Constantin Weißer <i7c@posteo.de>
2017-01-31 00:55:52 +0100

    Wip: PlayerStateMachine

---
+++ client/linux/Cfm/PlayerStateMachine.pm
@@ -0,0 +1,202 @@
+package Cfm::PlayerStateMachine;
+
+use strict;
+use warnings FATAL => 'all';
+use Moo;
+
+has artist => (is => 'rw');
+
+has title => (is => 'rw');
+
+has album => (is => 'rw');
+
+has length => (is => 'rw');
+
+# -1: stopped/neverplayed; 0: pause; 1: playing
+has state => (is => 'rw');
+
+has start_time => (is => 'rw');
+
+has passed_time => (is => 'rw');
+
+has threshold => (is => 'rw');
+
+has cb_playback_completed => (is => 'rw');
+
+has cb_playback_canceled => (is => 'rw');
+
+has cb_playback_started => (is => 'rw');
+
+has cb_playback_resumed => (is => 'rw');
+
+sub BUILD {
+    my ($self) = @_;
+
+    $self->state(- 1);
+    $self->threshold(0.5);
+}
+
+# Moves the machine into "playing" state. If the transition is from "stopped", a "begin of track" event will be fired
+#  and times will be reset accordingly. If transition is from "paused" or "playing", the outcome depends on whether
+# the track changes with this event: if yes, "end of track" will be fired as well as "begin of track", if no, a
+# "resume track" event is fired. Regardless of track change: the passed time will only be updated if state was not
+# "pause" before (in which case the pause transition already updated passed time). Start time will always be updated.
+sub play {
+    my $self = shift;
+    my $data = shift;
+    my @user_args = @_;
+
+    if ($self->state == - 1) {
+        # transition: stopped -> playing
+        $self->_set_new_track($data);
+        $self->_set_playing;
+        $self->_emit_begin_of_track_event(\@user_args);
+        return;
+    } elsif ($self->state == 0 || $self->state == 1) {
+        # transition: pause -> playing OR still playing
+        if ($self->_track_changed($data)) {
+            $self->_update_passed_time if ($self->state == 1);
+            $self->_emit_end_of_track_event(\@user_args);
+            $self->_set_new_track($data);
+            $self->_set_playing;
+            $self->_emit_begin_of_track_event(\@user_args);
+        } else {
+            $self->_update_passed_time if ($self->state == 1);
+            $self->_set_playing;
+            $self->_emit_resume_track_event(\@user_args);
+        }
+        return;
+    }
+    die "this should not happen: invalid state in PlayerStateMachine.";
+}
+
+# Shift state machine into "paused" state. This is illegal if state is stopped/never played. If the transition is
+# from "playing", the times will be updated. If this event involves a track change, "end of track" and "begin of
+# track" events will be fired.
+sub pause {
+    my $self = shift;
+    my $data = shift;
+    my @user_args = @_;
+
+    die "Illegal state for transition to pause" if ($self->state == - 1);
+
+    if ($self->state == 1) {
+        # transition: playing -> pause
+        $self->_update_passed_time;
+        $self->_set_paused;
+    } elsif ($self->state == 0) {
+        # do nothing?
+    }
+
+    if ($self->_track_changed($data)) {
+        $self->_emit_end_of_track_event(\@user_args);
+        $self->_set_new_track($data);
+        $self->_emit_begin_of_track_event($data);
+        return;
+    } else {
+        return;
+    }
+    die "this should not happen";
+}
+
+# update the passed time using the moment "now" and the start time of the playing period. This function is NOT
+# idempotent. You must reset the start time afterwards.
+sub _update_passed_time {
+    my ($self) = @_;
+
+    my $now = time();
+    $self->passed_time($self->passed_time + ($now - $self->start_time));
+}
+
+# sets internal state to "playing"; this involves setting the start time for the playing period.
+sub _set_playing {
+    my ($self) = @_;
+
+    $self->start_time(time());
+    $self->state(1);
+}
+
+# sets internal state to "paused"
+sub _set_paused {
+    my ($self) = @_;
+
+    $self->state(0);
+}
+
+# return true if track changed
+sub _track_changed {
+    my ($self, $data) = @_;
+
+    return 1 if (($self->artist ne $data->{artist})
+        || ($self->title ne $data->{title})
+        || ($self->album ne $data->{album}));
+    return 0;
+}
+
+# Sets the new track and resets passed time
+sub _set_new_track {
+    my ($self, $data) = @_;
+
+    $self->passed_time(0);
+    $self->artist($data->{artist});
+    $self->title($data->{title});
+    $self->album($data->{album});
+    $self->length($data->{length});
+}
+
+# emits an event for a track that is not played anymore; depending on the actual passed time this might result in a
+# "completed" event or a "canceled" event.
+sub _emit_end_of_track_event {
+    my ($self, $user_args) = @_;
+
+    if (($self->passed_time / $self->length) >= $self->threshold) {
+        if (defined $self->cb_playback_completed) {
+            $self->cb_playback_completed->($self->artist,
+                $self->title,
+                $self->album,
+                $self->length,
+                $self->passed_time,
+                @$user_args)
+        }
+    } else {
+        if (defined $self->cb_playback_canceled) {
+            $self->cb_playback_canceled->($self->artist,
+                $self->title,
+                $self->album,
+                $self->length,
+                $self->passed_time,
+                @$user_args)
+        }
+    }
+}
+
+# emits an event if a new track is played
+sub _emit_begin_of_track_event {
+    my ($self, $user_args) = @_;
+
+    if (defined $self->cb_playback_started) {
+        $self->cb_playback_started->($self->artist,
+            $self->title,
+            $self->album,
+            $self->length,
+            $self->passed_time,
+            @$user_args)
+    }
+}
+
+# emits an event if a track is played, that has already been playing before. This might happen if play is called in
+# "playing" or "paused" state, regardless, but no track change occured.
+sub _emit_resume_track_event {
+    my ($self, $user_args) = @_;
+
+    if (defined $self->cb_playback_resumed) {
+        $self->cb_playback_resumed->($self->artist,
+            $self->title,
+            $self->album,
+            $self->length,
+            $self->passed_time,
+            @$user_args)
+    }
+}
+
+1;
