250ac71547a7c6a209383938bcae497dd561ddcf
Constantin Weißer <i7c@posteo.de>
2017-03-03 17:03:11 +0000

    Execute create schema scripts

---
+++ mbslave/create_schemas.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir="$HOME/git/mbslave"
+[ ! -d  "$dir" ] && {
+	echo "Missing mbslave. Maybe you have to run setup_ec2.sh first.";
+	exit 1;
+}
+
+cd "$dir" || { echo "Failed."; exit 2; }
+
+echo 'CREATE SCHEMA musicbrainz;' | ./mbslave-psql.py -S
+echo 'CREATE SCHEMA statistics;' | ./mbslave-psql.py -S
+echo 'CREATE SCHEMA cover_art_archive;' | ./mbslave-psql.py -S
+echo 'CREATE SCHEMA wikidocs;' | ./mbslave-psql.py -S
+echo 'CREATE SCHEMA documentation;' | ./mbslave-psql.py -S
+
+./mbslave-remap-schema.py <sql/CreateTables.sql | ./mbslave-psql.py
+./mbslave-remap-schema.py <sql/statistics/CreateTables.sql | ./mbslave-psql.py
+./mbslave-remap-schema.py <sql/caa/CreateTables.sql | ./mbslave-psql.py
+./mbslave-remap-schema.py <sql/wikidocs/CreateTables.sql | ./mbslave-psql.py
+./mbslave-remap-schema.py <sql/documentation/CreateTables.sql | ./mbslave-psql.py
