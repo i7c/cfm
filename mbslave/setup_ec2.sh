307a2881265758596af290c5039f604d12719d4e
Constantin Weißer <i7c@posteo.de>
2017-03-03 11:55:01 +0000

    Script to install dependencies on EC2

---
+++ mbslave/setup_ec2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ sudo yum update
+ sudo yum install git tmux vim
+ sudo yum install python python-psycopg2 python-psycopg2 python27-psycopg2
+ sudo yum install postgresql-libs postgresql psql
+ sudo yum install buildutils coreutils gcc
