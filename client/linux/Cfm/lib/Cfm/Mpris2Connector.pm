abcfbd9105d4a300d9e684aa9f48c4a10e62d08f
Constantin Weißer <i7c@posteo.de>
2017-03-11 18:07:41 +0100

    Cli: logging in Mpris2Connector

--- client/linux/Cfm/lib/Cfm/Mpris2Connector.pm
+++ client/linux/Cfm/lib/Cfm/Mpris2Connector.pm
@@ -7,16 +7,19 @@ use Net::DBus;
 use Net::DBus::Reactor;
 use Data::Dumper;
 use Data::Printer;
+use Log::Log4perl;
 
 use Cfm::PlayerStateMachine;
 use Cfm::SavePlaybackDto;
+
+my $logger = Log::Log4perl->get_logger("cfm");
 
 has psm => (
         is      => 'rw',
         default => sub {
             Cfm::PlayerStateMachine->new(
                 cb_playback_started   => \&started,
-                cb_playback_canceled  => \&canceled,
+                cb_playback_canceled  => \&completed,
                 cb_playback_completed => \&completed,
                 cb_playback_resumed   => \&resumed
             );
@@ -27,17 +30,20 @@ has client => (is => 'ro');
 
 has dbus_name => (is => 'ro');
 
-has debug => (is => 'ro');
-
 sub listen {
     my ($self) = @_;
 
+    $logger->debug("Get DBus session");
     my $bus = Net::DBus->session;
+    $logger->debug("Get DBus service ".$self->dbus_name);
     my $spotify_service = $bus->get_service($self->dbus_name);
+    $logger->debug("Get DBus interface");
     my $mpris_interface = $spotify_service->get_object("/org/mpris/MediaPlayer2", "org.freedesktop.DBus.Properties");
+    $logger->debug("Hook to signal PropertiesChanged");
     $mpris_interface->connect_to_signal("PropertiesChanged", sub {
             my ($source, $rawdata) = @_;
 
+            $logger->info("Received signal from $source");
             my $metadata = $rawdata->{Metadata};
             for my $artist ($metadata->{"xesam:artist"}->@*) {
                 utf8::decode($artist);
@@ -45,14 +51,15 @@ sub listen {
             utf8::decode($metadata->{"xesam:title"});
             utf8::decode($metadata->{"xesam:album"});
             my $data = {
-                artists => $metadata->{"xesam:artist"},
-                title  => $metadata->{"xesam:title"},
-                album  => $metadata->{"xesam:album"},
-                length => $metadata->{"mpris:length"} / 1000, # in ms
+                artists     => $metadata->{"xesam:artist"},
+                title       => $metadata->{"xesam:title"},
+                album       => $metadata->{"xesam:album"},
+                length      => $metadata->{"mpris:length"} / 1000, # in ms
                 trackNumber => $metadata->{"xesam:trackNumber"},
-                discNumber => $metadata->{"xesam:discNumber"},
-                rawdata => $metadata
+                discNumber  => $metadata->{"xesam:discNumber"},
+                rawdata     => $metadata
             };
+            $logger->debug("Extracted metadata: ".Dumper($data));
 
             if ($rawdata->{PlaybackStatus} eq "Playing") {
                 $self->psm->play($data, $self);
@@ -60,7 +67,7 @@ sub listen {
                 $self->psm->pause($data, $self);
             }
         });
-    print "Listening.\n";
+    $logger->info("Connection to DBus established. Listening ...");
     my $reactor = Net::DBus::Reactor->main();
     $reactor->run();
 }
@@ -69,11 +76,9 @@ sub started {
     my ($metadata, $passed_time, $self) = @_;
     my $artist = join(", ", @{$metadata->{artists}});
 
-    if ($self->debug) {
-        p($metadata);
-        p($passed_time);
-    }
-    print "Started: $artist - $metadata->{title}\n";
+    $logger->debug(Dumper($metadata));
+    $logger->debug("Passed time: $passed_time");
+    $logger->info("Started: $artist - $metadata->{title}");
 }
 
 sub completed {
@@ -81,46 +86,39 @@ sub completed {
     my $artist = join(", ", @{$metadata->{artists}});
 
     my $create_playback = Cfm::SavePlaybackDto->new(
-        title   => $metadata->{title},
-        artists => $metadata->{artists},
-        album   => $metadata->{album},
-        length  => $metadata->{length},
-        trackNumber  => $metadata->{trackNumber},
+        title       => $metadata->{title},
+        artists     => $metadata->{artists},
+        album       => $metadata->{album},
+        length      => $metadata->{length},
+        trackNumber => $metadata->{trackNumber},
         discNumber  => $metadata->{discNumber},
     );
 
-    if ($self->debug) {
-        p($metadata);
-        p($passed_time);
-        p($create_playback);
-    }
+    $logger->debug(Dumper($metadata));
+    $logger->debug("Passed time: $passed_time");
+    $logger->debug("SavePlaybackDto: ".Dumper($create_playback->_to_hash));
 
-    print "Completed: $artist - $metadata->{title} ($passed_time of $metadata->{length} ms)\n";
-    print "Sending playback to server ... \n";
+    $logger->info("Completed: $artist - $metadata->{title} ($passed_time of $metadata->{length} ms)");
+    $logger->info("Sending playback to server ... ");
     $self->client->create_playback($create_playback);
-    print "OK\n";
 }
 
 sub canceled {
     my ($metadata, $passed_time, $self) = @_;
     my $artist = join(", ", @{$metadata->{artists}});
 
-    if ($self->debug) {
-        p($metadata);
-        p($passed_time);
-    }
-    print "Canceled: $artist - $metadata->{title} ($passed_time of $metadata->{length} ms)\n";
+    $logger->debug(Dumper($metadata));
+    $logger->debug("Passed time: $passed_time");
+    $logger->info("Canceled: $artist - $metadata->{title} ($passed_time of $metadata->{length} ms)");
 }
 
 sub resumed {
     my ($metadata, $passed_time, $self) = @_;
     my $artist = join(", ", @{$metadata->{artists}});
 
-    if ($self->debug) {
-        p($metadata);
-        p($passed_time);
-    }
-    print "Resumed: $artist - $metadata->{title} ($passed_time of $metadata->{length} ms)\n";
+    $logger->debug(Dumper($metadata));
+    $logger->debug("Passed time: $passed_time");
+    $logger->info("Resumed: $artist - $metadata->{title} ($passed_time of $metadata->{length} ms)");
 }
 
 1;
