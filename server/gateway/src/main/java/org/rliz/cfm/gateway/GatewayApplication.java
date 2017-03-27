7935fb3ace9c05f9a3d4a66568bfafb072a3e864
Constantin Weißer <i7c@posteo.de>
2017-03-27 14:01:02 +0200

    Server: Add gateway service

---
+++ server/gateway/src/main/java/org/rliz/cfm/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package org.rliz.cfm.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Main application.
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
