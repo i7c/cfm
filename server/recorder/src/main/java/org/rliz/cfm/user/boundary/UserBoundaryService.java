117ebab2157c0dd331dc8f197ef8f805ad9216ba
Constantin Weißer <i7c@posteo.de>
2017-07-30 19:19:17 +0200

    Rec: remove UserBoundaryServiceImpl
    Refactoring towards services without interfaces.

--- server/recorder/src/main/java/org/rliz/cfm/user/boundary/UserBoundaryService.java
+++ server/recorder/src/main/java/org/rliz/cfm/user/boundary/UserBoundaryService.java
@@ -1,18 +1,25 @@
 package org.rliz.cfm.user.boundary;
 
 import org.rliz.cfm.user.model.User;
+import org.rliz.cfm.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
- * Service for cfm {@link User}s.
+ * Implementation for {@link UserBoundaryService}
  */
-public interface UserBoundaryService {
+@Service
+public class UserBoundaryService {
 
-    /**
-     * Gets a user by their name
-     *
-     * @param name username
-     * @return user or null if not found
-     */
-    User getUserByUsername(String name);
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserBoundaryService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByUsername(String name) {
+        return userRepository.findOneByUsername(name);
+    }
 
 }
