ec684807807cd25d17ad4f8e149d7dc1ece8a8e0
Constantin Weißer <i7c@posteo.de>
2017-10-12 01:40:43 +0200

    Add UUID in user resource
    The user might be interested in their own UUID which is why we add them
    to the user resource. The database-internal oid will never be exposed.

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/user/api/UserTranslations.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/user/api/UserTranslations.kt
@@ -5,4 +5,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 fun User.toRes(status: HttpStatus): ResponseEntity<UserRes> =
-        ResponseEntity(UserRes(name = this.name, password = null, state = this.state), status)
+        ResponseEntity(UserRes(
+                name = this.name,
+                password = null,
+                state = this.state,
+                uuid = this.uuid
+        ), status)
