51b2ffa5714902bd4509a230c72428b2ee02bec1
Constantin Weißer <i7c@posteo.de>
2017-10-13 01:09:12 +0200

    Translate AccessDeniedException into error message
    To avoid the cluttered json, AccessDeniedException are translated into
    their messages and appropriate error code.

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/exception/ExceptionHandler.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package org.rliz.cfm.recorder.common.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
@@ -11,4 +12,6 @@ class ExceptionHandler {
     @ExceptionHandler(NotFoundException::class)
     fun notFound(e: NotFoundException): ResponseEntity<String> = e.toRes(HttpStatus.NOT_FOUND)
 
+    @ExceptionHandler(AccessDeniedException::class)
+    fun accessDenied(e: AccessDeniedException): ResponseEntity<String> = e.toRes(HttpStatus.FORBIDDEN)
 }
