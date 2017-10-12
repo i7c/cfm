343842f63273076d4b445d84a243fbf7128a9d35
Constantin Weißer <i7c@posteo.de>
2017-10-12 14:12:53 +0200

    Add System Initializer
    When starting the application, the system initializer will automatically
    execute certain setup tasks. The only task performed currently is
    checking whether a system user is present, and if not, add one.
    
    The name and password of the system user can be set with the properties:
    
    cfm.system-user
    cfm.systemm-password

---
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/lifecycle/SystemInit.kt
@@ -0,0 +1,55 @@
+package org.rliz.cfm.recorder.common.lifecycle
+
+import org.rliz.cfm.recorder.user.boundary.UserBoundary
+import org.rliz.cfm.recorder.user.data.UserState
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.SmartLifecycle
+import org.springframework.stereotype.Component
+
+val PHASE_SYSTEM_INIT = 100
+
+@Component
+class SystemInit : SmartLifecycle {
+
+    private var running = false
+
+    @Autowired
+    lateinit var userBoundary: UserBoundary
+
+    @Value("\${cfm.system-user:cfmadmin}")
+    lateinit var systemUserName: String
+
+    @Value("\${cfm.system-password:changeme}")
+    lateinit var systemPassword: String
+
+    override fun isRunning(): Boolean = running
+
+    override fun start() {
+        if (running) throw RuntimeException("SystemInit is already running")
+
+        systemUserInit()
+
+        running = true
+    }
+
+    override fun isAutoStartup(): Boolean = true
+
+    override fun stop(callback: Runnable?) {
+        running = false
+        callback!!.run()
+    }
+
+    override fun stop() {
+        running = false
+    }
+
+    override fun getPhase(): Int = PHASE_SYSTEM_INIT
+
+    private fun systemUserInit() {
+        val systemUser = userBoundary.findUserByName(systemUserName)
+        if (systemUser == null) {
+            userBoundary.createUser(systemUserName, systemPassword, UserState.ACTIVE)
+        }
+    }
+}
