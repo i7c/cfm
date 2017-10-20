491f79d8d6c8e8e8dc0501289f9980997f76fe66
Constantin Weißer <i7c@posteo.de>
2017-10-20 16:29:36 +0200

    Increase error traceability
    This patch increases traceability of errors in two ways:
    
    1. The user gets a server-side timestamp along with the error message,
       so that if they report an error, we can more easily find the right
       spot in the logs.
    2. For all uncaught exceptions (default exception handler) the entire
       stack trace is logged on INFO.

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/exception/ExceptionHandler.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/exception/ExceptionHandler.kt
@@ -1,5 +1,6 @@
 package org.rliz.cfm.recorder.common.exception
 
+import org.rliz.cfm.recorder.common.log.logger
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.AccessDeniedException
@@ -9,12 +10,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class ExceptionHandler {
 
+    companion object {
+        val log = logger<ExceptionHandler>()
+    }
+
+    fun <E: Exception> logged(e: E): E {
+        log.info("Uncaught exception", e)
+        return e
+    }
+
     @ExceptionHandler(NotFoundException::class)
-    fun notFound(e: NotFoundException): ResponseEntity<String> = e.toRes(HttpStatus.NOT_FOUND)
+    fun notFound(e: NotFoundException): ResponseEntity<String> = logged(e).toRes(HttpStatus.NOT_FOUND)
 
     @ExceptionHandler(AccessDeniedException::class)
-    fun accessDenied(e: AccessDeniedException): ResponseEntity<String> = e.toRes(HttpStatus.FORBIDDEN)
+    fun accessDenied(e: AccessDeniedException): ResponseEntity<String> = logged(e).toRes(HttpStatus.FORBIDDEN)
 
     @ExceptionHandler
-    fun defaultHandler(e: Exception): ResponseEntity<String> = e.toRes(HttpStatus.BAD_REQUEST)
+    fun defaultHandler(e: Exception): ResponseEntity<String> = logged(e).toRes(HttpStatus.BAD_REQUEST)
 }
--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/exception/ExceptionTranslations.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/common/exception/ExceptionTranslations.kt
@@ -2,6 +2,14 @@ package org.rliz.cfm.recorder.common.exception
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 fun Exception.toRes(status: HttpStatus): ResponseEntity<String> =
-        ResponseEntity(this.message ?: "Fatal ${this::class.qualifiedName} without message", status)
+        if (this.message == null)
+            ResponseEntity("Fatal ${this::class.qualifiedName} without message", status)
+        else
+            ResponseEntity(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mmX")
+                    .withZone(ZoneOffset.UTC)
+                    .format(Instant.now()) + ": ${this.message}", status)
