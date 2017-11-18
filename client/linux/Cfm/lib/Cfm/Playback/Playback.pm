530e3dfd554b0e40641c53a151d944515ed1974e
Constantin Weißer <i7c@posteo.de>
2017-11-18 15:17:41 +0100

    Add validation to Playback resource
    Adds a valid() sub to Playback which returns true if the Playback can be
    considered a valid Playback Resource. For that, the artist field is
    coerced to a list without any undefined values in it. Such undefined
    values could be added if the Playback is filled by the user.

--- client/linux/Cfm/lib/Cfm/Playback/Playback.pm
+++ client/linux/Cfm/lib/Cfm/Playback/Playback.pm
@@ -19,7 +19,15 @@ with "Cfm::Common::Res";
     broken => \&Cfm::Common::Res::_ds_boolean
 );
 
-has artists => (is => 'ro');
+has artists => (
+        is     => 'ro',
+        coerce => sub {
+            [ grep defined, $_[0]->@* ]
+        },
+        isa    => sub {
+            die "artists must be a list" unless ref $_[0] eq 'ARRAY'
+        },
+    );
 has recordingTitle => (is => 'ro');
 has releaseTitle => (is => 'ro');
 has timestamp => (is => 'ro');
@@ -29,5 +37,13 @@ has discNumber => (is => 'ro');
 has trackNumber => (is => 'ro');
 has broken => (is => 'ro');
 has id => (is => 'ro');
+
+sub valid {
+    my ($self) = @_;
+
+    return defined $self->artists && scalar $self->artists->@* > 0
+            && defined $self->recordingTitle
+            && defined $self->releaseTitle;
+}
 
 1;
