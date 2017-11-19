48f51f5b41bada122ed31305e093dc97da432a65
Constantin Weißer <i7c@posteo.de>
2017-11-19 21:22:01 +0100

    Remove useless boundary interface

--- server/mbservice/src/main/java/org/rliz/mbs/recording/boundary/RecordingBoundaryService.java
+++ server/mbservice/src/main/java/org/rliz/mbs/recording/boundary/RecordingBoundaryService.java
@@ -1,44 +1,77 @@
 package org.rliz.mbs.recording.boundary;
 
+import org.rliz.mbs.common.exception.MbEntityNotFoundException;
+import org.rliz.mbs.common.exception.MbLookupException;
+import org.rliz.mbs.rating.model.Rated;
+import org.rliz.mbs.rating.service.RatingService;
 import org.rliz.mbs.recording.model.Recording;
+import org.rliz.mbs.recording.repository.RecordingRepository;
 import org.rliz.mbs.release.model.Release;
-import org.rliz.mbs.release.model.ReleaseGroup;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-/**
- * Service for {@link Recording}s.
- */
-public interface RecordingBoundaryService {
+@Service
+public class RecordingBoundaryService {
 
-    /**
-     * Tries to identify a {@link Recording} using given release the recording appears on and title of the recording.
-     *
-     * @param release   release this recording was played from
-     * @param title     title of the recording
-     * @param threshold min score to consider the result a match
-     * @return the identified recording
-     */
-    Recording identifyRecording(Release release, String title, int threshold);
+    @Autowired
+    private RecordingRepository recordingRepository;
 
-    /**
-     * Retrieve single {@link Recording} by identifier.
-     *
-     * @param identifier unique identifier
-     * @return the found {@link Recording}
-     */
-    Recording findByIdentifier(UUID identifier);
+    @Autowired
+    private RatingService ratingService;
 
-    /**
-     * Lookup a recording with given {@link ReleaseGroup} on which the recording appears and the name of the recording.
-     *
-     * @param releaseGroupId the identifier of the release group
-     * @param name           the name of the recording
-     * @param pageable       page request
-     * @return list of recordings ordered by rating
-     */
-    Page<Recording> findRecordingByReleaseGroupIdentifierAndName(UUID releaseGroupId, String name, Pageable pageable);
+    public Recording identifyRecording(Release release, String title, int threshold) {
+        List<Recording> recordings = recordingRepository.findAllByReleaseGroup(release.getReleaseGroup());
+
+        if (CollectionUtils.isEmpty(recordings)) {
+            throw new MbLookupException(MbLookupException.EC_NO_RESULTS,
+                    "No recordings found for release with UUID %s.", release.getIdentifier());
+        }
+
+        List<Rated<Recording>> ratedRecordings = ratingService.rateRecordings(recordings, title);
+        Rated<Recording> candidate = ratedRecordings.get(0);
+        if (candidate.getRating() >= threshold) {
+            return candidate.getIt();
+        } else {
+            throw new MbLookupException(MbLookupException.EC_LOW_RATING, "All found recordings were rated below " +
+                    "threshold.");
+        }
+    }
+
+    public Recording findByIdentifier(UUID identifier) {
+        Recording foundRecording = recordingRepository.findByIdentifier(identifier);
+        if (foundRecording == null) {
+            throw new MbEntityNotFoundException(MbEntityNotFoundException.EC_NO_SUCH_UUID,
+                    "No recording found for UUID %s.", identifier);
+        }
+        return foundRecording;
+    }
+
+    public Page<Recording> findRecordingByReleaseGroupIdentifierAndName(UUID releaseGroupId, String name,
+                                                                        Pageable pageable) {
+        List<Recording> foundRecordings = recordingRepository.findAllByReleaseGroupIdentifier(releaseGroupId);
+
+        if (CollectionUtils.isEmpty(foundRecordings)) {
+            throw new MbLookupException(MbLookupException.EC_NO_RESULTS,
+                    "No recordings found for release group with UUID %s.", releaseGroupId);
+        }
+
+        List<Rated<Recording>> ratedRecordings = ratingService.rateRecordings(foundRecordings, name);
+        List<Recording> recordings = ratedRecordings.stream().map(Rated::getIt).collect(Collectors.toList());
+        if (pageable.getOffset() >= recordings.size()) {
+            return new PageImpl<>(new ArrayList<>(), pageable, recordings.size());
+        }
+        int first = pageable.getOffset();
+        int last = Math.min(first + pageable.getPageSize(), recordings.size());
+        return new PageImpl<Recording>(recordings.subList(first, last), pageable, recordings.size());
+    }
 }
+
