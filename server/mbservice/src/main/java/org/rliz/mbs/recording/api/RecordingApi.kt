e3681a47ac07911e906cddebb61de3f2c0379448
Constantin Weißer <i7c@posteo.de>
2017-11-23 21:55:40 +0100

    Get rid of factories for GET Recordings
    Use transformer function instead. Throw away all unused factories and
    old POJOs.

--- server/mbservice/src/main/java/org/rliz/mbs/recording/api/RecordingApi.kt
+++ server/mbservice/src/main/java/org/rliz/mbs/recording/api/RecordingApi.kt
@@ -1,11 +1,12 @@
 package org.rliz.mbs.recording.api
 
+import org.rliz.mbs.common.api.toRes
 import org.rliz.mbs.recording.boundary.RecordingBoundaryService
+import org.rliz.mbs.recording.data.Recording
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.domain.Pageable
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
@@ -15,9 +16,18 @@ class RecordingApi {
     @Autowired
     private lateinit var recordingBoundary: RecordingBoundaryService
 
+    @Transactional(readOnly = true)
     @RequestMapping(method = arrayOf(RequestMethod.GET), path = arrayOf("/{uuid}"))
     fun getRecording(@PathVariable("uuid") uuid: UUID): RecordingRes =
             recordingBoundary.findByIdentifier(uuid)
                     .toRes()
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = arrayOf(RequestMethod.GET))
+    fun getRecordings(@RequestParam("releaseGroupId") releaseGroupId: UUID,
+                      @RequestParam("title") title: String,
+                      pageable: Pageable) =
+            recordingBoundary.findRecordingByReleaseGroupIdentifierAndName(releaseGroupId, title, pageable)
+                    .toRes(Recording::toRes)
 }
 
