6795fafc6cf51fdeb996a93ffb7a305a1e1b96b5
Constantin Weißer <i7c@posteo.de>
2017-11-24 21:55:46 +0100

    Move transformer function to resource class
    They sort of belong together, since one produces the other. This
    probably is followed by some refactoring, because there are several
    transformer functions that are stored in separate files.

--- server/mbservice/src/main/java/org/rliz/mbs/recording/api/RecordingRes.kt
+++ server/mbservice/src/main/java/org/rliz/mbs/recording/api/RecordingRes.kt
@@ -1,7 +1,15 @@
 package org.rliz.mbs.recording.api
 
+import org.rliz.mbs.recording.data.Recording
+
 data class RecordingRes(
         val name: String? = null,
         val length: Long? = null,
         val artists: List<String> = emptyList()
+)
+
+fun Recording.toRes() = RecordingRes(
+        name = this.name,
+        length = this.length,
+        artists = this.artistCredit.artistCreditName.map { it.artist.name }
 )
--- server/mbservice/src/main/java/org/rliz/mbs/recording/api/Trans.kt
+++
@@ -1,9 +0,0 @@
-package org.rliz.mbs.recording.api
-
-import org.rliz.mbs.recording.data.Recording
-
-fun Recording.toRes() = RecordingRes(
-        name = this.name,
-        length = this.length,
-        artists = this.artistCredit.artistCreditName.map { it.artist.name }
-)
