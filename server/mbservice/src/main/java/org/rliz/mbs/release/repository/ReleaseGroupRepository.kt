824ca0a1e5d8a0ad105f6ebb381cd0466aebf213
Constantin Weißer <i7c@posteo.de>
2017-11-26 01:19:48 +0100

    Convert ReleaseGroupRepository to Kotlin interface

---
+++ server/mbservice/src/main/java/org/rliz/mbs/release/repository/ReleaseGroupRepository.kt
@@ -0,0 +1,26 @@
+package org.rliz.mbs.release.repository
+
+import org.rliz.mbs.release.model.ReleaseGroup
+import org.springframework.data.jpa.repository.EntityGraph
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.*
+
+interface ReleaseGroupRepository : JpaRepository<ReleaseGroup, Long> {
+
+    @EntityGraph(attributePaths = arrayOf("artistCredit.artistCreditName.artist"))
+    fun findByIdentifier(identifier: UUID): ReleaseGroup
+
+    @Query("""
+        SELECT r FROM
+        ReleaseGroup r
+        join r.artistCredit ac
+        join ac.artistCreditName acn
+        join acn.artist a
+        where lower(a.name) in (:artistNames)
+        """)
+    @EntityGraph(attributePaths = arrayOf("artistCredit.artistCreditName.artist"))
+    fun findReleaseGroupByArtistNames(@Param("artistNames") artistNames: List<String>): List<ReleaseGroup>
+
+}
