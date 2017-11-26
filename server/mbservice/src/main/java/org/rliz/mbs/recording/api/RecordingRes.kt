1236f5741823bd0e7bc9c9b295f233436ac21a8c
Constantin Weißer <i7c@posteo.de>
2017-11-26 01:27:47 +0100

    Add ID to Recording and ReleaseGroup resources
    When requesting multiple recordings (or releasegroups) in one request,
    the client will need to do a mapping of the elements in the result. The
    unique identifier will be the key for this mapping, so we have to add it
    to the resource.

--- server/mbservice/src/main/java/org/rliz/mbs/recording/api/RecordingRes.kt
+++ server/mbservice/src/main/java/org/rliz/mbs/recording/api/RecordingRes.kt
@@ -1,14 +1,17 @@
 package org.rliz.mbs.recording.api
 
 import org.rliz.mbs.recording.data.Recording
+import java.util.*
 
 data class RecordingRes(
-        val name: String? = null,
+        val id: UUID,
+        val name: String,
         val length: Long? = null,
         val artists: List<String> = emptyList()
 )
 
 fun Recording.toRes() = RecordingRes(
+        id = this.identifier,
         name = this.name,
         length = this.length,
         artists = this.artistCredit.artistCreditName.map { it.artist.name }
