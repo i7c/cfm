9c1ef4e995064e646ecf9182eb8698ed0f6b7116
Constantin Weißer <i7c@posteo.de>
2017-12-01 01:00:11 +0100

    Delete unused classes
    These are leftovers from the time when we replicated musicbrainz artists
    in the recorder.

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/artist/data/ArtistRepo.kt
+++
@@ -1,9 +0,0 @@
-package org.rliz.cfm.recorder.artist.data
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface ArtistRepo : JpaRepository<Artist, Long> {
-
-    fun findByUuidIn(uuids: List<UUID>): List<Artist>
-}
