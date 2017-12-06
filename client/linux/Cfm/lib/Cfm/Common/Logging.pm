e8616a818f77adce86600ec19b83bac0c56cb963
Constantin Weißer <i7c@posteo.de>
2017-12-06 23:52:45 +0100

    Register bean to reset log level
    This patch registers a bean "loglevel" during the setup of the logger.
    The bean provides a sub that can be called to reset the log level.
    Elsewhere in the code one can inject the bean if there's need to reset
    the log level, which defaults to WARN.
    
    In case that the log level was set via environment variable, we register
    a NOP-sub that does nothing instead. That way we prevent that some code
    resets the log level again, if the user specifically requested a
    particular level.
    
    We specifically do not provide the logger directly, because we want to
    abstract the exact implementation of the logger.

--- client/linux/Cfm/lib/Cfm/Common/Logging.pm
+++ client/linux/Cfm/lib/Cfm/Common/Logging.pm
@@ -6,6 +6,15 @@ use Log::Log4perl::Layout;
 use Log::Log4perl::Level;
 use Log::Any::Adapter;
 
+use Cfm::Bean;
+
+my %levels = (
+    debug => $DEBUG,
+    info  => $INFO,
+    warn  => $WARN,
+    error => $ERROR,
+);
+
 sub setup_logger {
     my $logger = Log::Log4perl->get_logger("");
     my $layout = Log::Log4perl::Layout::PatternLayout->new("%d %5r [%c] %L %m%n");
@@ -26,21 +35,29 @@ sub setup_logger {
     $logger->add_appender($stdout_appender);
     # environment variable CFM_LL can be used to reset log level before bootstrapping
     if (defined $ENV{CFM_LL}) {
-        my $level = $ENV{CFM_LL};
-        if ($level eq "info") {
-            $logger->level($INFO);
-        } elsif ($level eq "debug") {
-            $logger->level($DEBUG);
-        } elsif ($level eq "error") {
-            $logger->level($ERROR);
-        } elsif ($level eq "warn") {
-            $logger->level($WARN);
-        }
+        my $l = $ENV{CFM_LL};
+
+        $logger->level($levels{$l}) if defined $levels{$l};
+        # If the log level is set by an environment variable, we do not allow any code to reset the level again. Thus
+        # we register a sub that does nothing as bean.
+        bean loglevel => sub {sub {1;}};
     } else {
         $logger->level($WARN); # default level
+        # We register a closure as bean, which can be used to reset the log level. We could as well put the logger
+        # directly into the bean, but that would expose logging implementation details to the client code. The sub
+        # abstracts the details of the logging implementation. Whenever logging is setup, one can inject loglevel
+        # anywhere and call the sub.
+        bean loglevel => sub {
+                sub {
+                    my ($l) = @_;
+
+                    $logger->level($levels{$l}) if defined $levels{$l};
+                }
+            };
     }
     Log::Any::Adapter->set('Log4perl');
     $logger->debug("Log initialisation completed");
+
     return $logger;
 }
 
