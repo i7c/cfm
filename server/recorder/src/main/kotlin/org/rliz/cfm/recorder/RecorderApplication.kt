52a39408e4d7b193df1d6dc8da29725f8e963fc9
Constantin Weißer <i7c@posteo.de>
2018-04-04 23:54:47 +0200

    rec: use ComponentScan instead of SpringBootApplication
    The latter was cause of some problems with IntelliJ lately.

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/RecorderApplication.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/RecorderApplication.kt
@@ -2,10 +2,10 @@ package org.rliz.cfm.recorder
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan("org.rliz.cfm")
 class RecorderApplication
 
 fun main(args: Array<String>) {
