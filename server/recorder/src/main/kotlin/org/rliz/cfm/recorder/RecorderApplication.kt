9e25d780c1a2feec7015999b55751ddc819b6994
Constantin Weißer <i7c@posteo.de>
2018-05-11 18:37:57 +0200

    rec: Introduce scheduled auto-fixer
    Add scheduled automatic fixing of unattached playbacks. This is meant to
    replace the manual fixing done that could be triggered by users via the
    REST API so far. In the future, we can drop support for manual fixing.

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/RecorderApplication.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/RecorderApplication.kt
@@ -3,9 +3,11 @@ package org.rliz.cfm.recorder
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableAutoConfiguration
 @ComponentScan("org.rliz.cfm")
+@EnableScheduling
 class RecorderApplication
 
 fun main(args: Array<String>) {
---
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/playback/scheduler/AutoFixer.kt
@@ -0,0 +1,64 @@
+package org.rliz.cfm.recorder.playback.scheduler
+
+import org.rliz.cfm.recorder.mbs.service.MbsService
+import org.rliz.cfm.recorder.playback.boundary.PlaybackBoundary
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.TimeUnit
+
+@Service
+class AutoFixer {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(AutoFixer::class.java)
+        private const val ATTEMPT_BACKOFF_HOURS: Long = 6
+        private const val BATCH_SIZE = 1000
+    }
+
+    @Autowired
+    private lateinit var playbackBoundary: PlaybackBoundary
+
+    @Autowired
+    private lateinit var mbs: MbsService
+
+    @Scheduled(fixedDelay = 3 * 60 * 1000)
+    fun fixBunch() {
+        log.info("Starting auto-fixer run ...")
+        val unattached = playbackBoundary.getUnattachedPlaybackGroups(
+            BATCH_SIZE,
+            Instant.now().minus(Duration.ofHours(ATTEMPT_BACKOFF_HOURS)).epochSecond
+        )
+        if (unattached.isEmpty()) {
+            log.info("Nothing to fix, return early.")
+            return
+        }
+
+        unattached.map { group ->
+            val (rgId, recId) = try {
+                mbs.identifyPlayback(
+                    group.artists[0],
+                    group.releaseTitle,
+                    group.recordingTitle,
+                    group.length ?: 0
+                ).get(5, TimeUnit.SECONDS).let { Pair(it.releaseGroupId, it.recordingId) }
+            } catch (e: ExecutionException) {
+                Pair(null, null)
+            }
+            playbackBoundary.updateMbsOnPlaybackGroup(
+                group.artists,
+                group.releaseTitle,
+                group.recordingTitle,
+                group.length,
+                rgId,
+                recId
+            )
+        }.sum().apply {
+            log.info("Finished run, updated $this playbacks.")
+        }
+    }
+}
