7a94c0cb992f0df3ee279fa0970ba5e7c575d7ea
Constantin Weißer <i7c@posteo.de>
2018-05-13 23:58:27 +0200

    wip

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/RecorderApplication.kt
+++ server/recorder/src/main/kotlin/org/rliz/cfm/recorder/RecorderApplication.kt
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableAutoConfiguration
 @ComponentScan("org.rliz.cfm")
-@EnableScheduling
 class RecorderApplication
 
 fun main(args: Array<String>) {
