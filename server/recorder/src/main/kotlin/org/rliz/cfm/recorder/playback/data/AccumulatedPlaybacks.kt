243b092f6026d8f0db429c87e207dcf7fa5b1c73
Constantin Weißer <i7c@posteo.de>
2018-07-07 14:55:12 +0200

    rec: Delete unused classes

--- server/recorder/src/main/kotlin/org/rliz/cfm/recorder/playback/data/AccumulatedPlaybacks.kt
+++
@@ -1,9 +0,0 @@
-package org.rliz.cfm.recorder.playback.data
-
-data class AccumulatedPlaybacks(
-    val occurrences: Long?,
-    val artistsJson: String?,
-    val recordingTitle: String?,
-    val releaseTitle: String?,
-    val fixAttempt: Long?
-)
